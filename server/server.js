72507a756b50c29808805b1776b6d90134d52765
lolalolabob123 <CALLUM.CANDY23@bathspa.ac.uk>
2025-11-11 22:36:08 +0000

    fixed file locations

--- server/server.js
+++ server/server.js
@@ -12,12 +12,14 @@ app.listen(port, () => {
 //Root to handle incoming requests
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/views/index.html'))
+    console.log('__dirname:', __dirname);
+    console.log('Full path:', path.join(__dirname, '../client/views/index.html'));
 })
 
 const data = [
-    {id: 1, name: 'Restaurant 1'},
-    {id: 2, name: 'Restaurant 2'},
-    {id: 3, name: 'Restaurant 3'}
+    { id: 1, name: 'Restaurant 1' },
+    { id: 2, name: 'Restaurant 2' },
+    { id: 3, name: 'Restaurant 3' }
 ]
 
 //Middleware to parse JSON requests
@@ -26,7 +28,7 @@ app.use(express.urlencoded())
 
 //Create a new restaurant
 app.post('/addRestaurant', (req, res) => {
-    let restaurantID = data.length+1
+    let restaurantID = data.length + 1
     const restaurantName = req.body.name
     let newRestaurant = {
         id: restaurantID,
@@ -39,16 +41,16 @@ app.post('/addRestaurant', (req, res) => {
 
 //Read all restaurants
 app.get('/getRestaurants', (req, res) => {
-    res.json({restaurants: data})
+    res.json({ restaurants: data })
 })
 
 //Update a restaurant by ID
 app.get('/restaurant/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const restaurant = data.find((restaurant) => restaurant.id === id)
-    if (!restaurant){
-        res.status(404).json({error: 'Restaurant not found'})
-    } else{
+    if (!restaurant) {
+        res.status(404).json({ error: 'Restaurant not found' })
+    } else {
         res.json(data[index])
     }
 })
@@ -56,10 +58,10 @@ app.get('/restaurant/:id', (req, res) => {
 //Delete a restaurant by ID
 app.delete('/deleteRestaurant/:id', (req, res) => {
     const id = parseInt(req.params.id)
-    const index = data .findIndex((restaurant) => restaurant.id === id)
-    if (index === -1){
-        res.status(404).json({error:  'Restaurant not found'})
-    } else{
+    const index = data.findIndex((restaurant) => restaurant.id === id)
+    if (index === -1) {
+        res.status(404).json({ error: 'Restaurant not found' })
+    } else {
         const deleteRestaurant = data.splice(index, 1)
         res.json(deleteRestaurant[0])
     }
