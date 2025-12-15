b4a8bbb4012fb08243c8ccc1bdf5c307aee45883
lolalolabob123 <CALLUM.CANDY23@bathspa.ac.uk>
2025-12-15 14:46:45 +0000

    fixed close button positioning

--- client/src/pages/Modal.jsx
+++ client/src/pages/Modal.jsx
@@ -241,7 +241,7 @@ const Modal = ({ isOpen, onClose, name, image, restaurantId, onImageUpdate, onNa
                     </>
                 )}
 
-                <button className="modal-close" onClick={onClose}>✕</button>
+                {/* <button className="modal-close" onClick={onClose}>✕</button> */}
             </div>
         </div>
     );
